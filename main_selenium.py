0c12f394fc83833071fcc55e4d5fb5cacfcb6135
Noru <akang8150@naver.com>
2022-12-27 13:29:03 +0000

    prepare post

--- main_selenium.py
+++ main_selenium.py
@@ -1,47 +1,32 @@
 from time import time
 from datetime import datetime
-import urllib3
 from urllib.parse import urlparse, parse_qs
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
-
+from config.options import options
 
 base_url = "https://www.boannews.com/media"
 
+browser = webdriver.Chrome(options=options)
+
 start = time()
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-response = requests.get(f"{base_url}/list.asp?mkind=1", verify=False)
-
-
-soup = BeautifulSoup(response.text, "html.parser")
+browser.get(f"{base_url}/list.asp?mkind=1")
+soup = BeautifulSoup(browser.page_source, "html.parser")
 news = soup.find("div", id="news_area").find_all("div", class_="news_list")
 end = time()
 results = []
-n = 0
 for new in news:
     anchor = new.find("a")
     idx = parse_qs(urlparse(anchor["href"]).query)["idx"][0]
     title = anchor.text.strip()
     writer, pub_date = new.find("span", class_="news_writer").text.split(" | ")
     pub_date = datetime.strptime(pub_date, "%Y년 %m월 %d일 %H:%M")
-
-    post_response = requests.get(f"{base_url}/view.asp?idx={idx}", verify=False)
-    post_soup = BeautifulSoup(post_response.text, "html.parser")
-    news_content = post_soup.find("div", id="news_content")
-    content = news_content.text
     new_data = {
         "idx": idx,
         "link": f"{base_url}/view.asp?idx={idx}",
-        "title": title,
         "writer": writer.replace(" 기자", ""),
         "pub_date": pub_date.strftime("%Y-%m-%d %H:%M"),
-        "content": content,
     }
-
     results.append(new_data)
-    print(n)
-    n += 1
-
-for result in results:
-    print(result, sep="\n\n")
+    print(results)
 print(f"{end - start:.4f}초")
